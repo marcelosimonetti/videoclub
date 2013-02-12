29bbe6030558a5f5d2e57344e4c6f3e90b3f5751
marcelo simonetti <marcelosimonetti10@gmail.com>
2013-02-12 20:24:05 -0300

    esqueleto cakephp

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'videoclub',
+		'password' => 'videoclub',
+		'database' => 'videoclub',
+		'encoding' => 'utf8'
+	);
+}
